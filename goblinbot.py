946c5bec80cc539ca4e638991388a62740669422
Eddie Bracho <eddiebracho@gmail.com>
2016-08-31 06:14:29 +0000

    Replaced average price with median price in report

--- goblinbot.py
+++ goblinbot.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import statistics
 import requests
 import asyncio
 import discord
@@ -33,6 +34,18 @@ def match_realm(region, name):
     return next( (realm for realm in realms if realm.lower().startswith(name.lower())), None)
 
 
+import json
+def tuj_item(house, item_id):
+    params = {
+        'house': house,
+        'item': item_id
+    }
+    r = requests.get('https://theunderminejournal.com/api/item.php', params=params)
+    auction_data = r.json()['auctions'][0]['data']
+    buyouts = [ ad['buy'] for ad in auction_data ]
+    return buyouts
+
+
 def tuj_search(region, realm, item):
     if region not in ['us', 'eu']:
         raise Exception('Invalid region. Must be "us" or "eu"')
@@ -50,16 +63,22 @@ def tuj_search(region, realm, item):
         'house': house,
         'search': item
     }
+
     r = requests.get('https://theunderminejournal.com/api/search.php', params=params)
+
     items = r.json()['items']
     if items is None:
         raise Exception('Item not found, did you spell it correctly?')
+
     stats = items[0]
+    buyouts = tuj_item(house, stats['id'])
+    stats['medianprice'] = "{0:.2f}g".format(statistics.median_low(buyouts)/10000)
+    stats['price'] = "{0:.2f}g".format(stats['price']/10000)
     stats['realm'] = realm
-    stats['price'] = "%.2fg" % (stats['price']/10000)
-    stats['avgprice'] = "%.2fg" % (stats['avgprice']/10000)
     stats['url'] = 'https://theunderminejournal.com/#{0}/{1}/item/{2}'.format(region, realm.lower(), stats['id'])
+
     return stats
+
 
 
 client = discord.Client()
@@ -86,11 +105,12 @@ async def on_message(message):
             print(e)
             await client.send_message(message.channel, str(e))
             return
-        stat_msg = '{0} {1} - Price: {2}, Average Price: {3}, Quantity: {4} {5}'.format(
-            stats['realm'], stats['name_enus'], stats['price'], stats['avgprice'], 
+        stat_msg = '{0} {1} - Min Price: {2}, Median Price: {3}, Quantity: {4} {5}'.format(
+            stats['realm'], stats['name_enus'], stats['price'], stats['medianprice'], 
             stats['quantity'], stats['url'])
         await client.send_message(message.channel, stat_msg)
 
 
-client.run(os.environ['WOWPCTOKEN'])
+if __name__ == '__main__':
+    client.run(os.environ['WOWPCTOKEN'])
 
